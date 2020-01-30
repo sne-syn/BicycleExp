0564deea9686c54e9addacab774250fdc6384096
sne <snezhana.syngur@gmail.com>
2020-01-30 12:19:29 +0100

    Finish home page

---
+++ source/js/modal.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function () {
+  var popup = document.querySelector('.modal');
+  var overlay = document.querySelector('.modal__overlay');
+  var openButtons = document.querySelectorAll('.modal__open-btn');
+  var closeButton = popup.querySelector('.modal__close');
+
+  var openPopup = function () {
+    popup.classList.add("modal--show");
+    document.addEventListener('keydown', popupEscHandler);
+  };
+
+  var closePopup = function () {
+    popup.classList.remove('modal--show');
+    document.activeElement.blur();
+    document.removeEventListener('keydown', popupEscHandler);
+  };
+
+  openButtons.forEach(function (button) {
+    button.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      openPopup();
+    });
+  });
+
+  openButtons.forEach(function (button) {
+    button.addEventListener("keydown", function (evt) {
+      window.util.keyaction.addEnterEvent(evt, openPopup());
+    });
+  });
+
+  closeButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal--show");
+  });
+
+  var popupEscHandler = function (evt) {
+    window.util.keyaction.addEscEvent(evt,
+      closePopup);
+  };
+
+  var closeButtonClickHandler = function (evt) {
+    evt.preventDefault();
+    closePopup();
+  };
+
+  closeButton.addEventListener('click', closeButtonClickHandler);
+  overlay.addEventListener('click', closePopup);
+
+})();
