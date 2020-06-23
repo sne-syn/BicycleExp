a82d100e591f814aeef2d4b805cf295def951e19
sne <snezhana.syngur@gmail.com>
2020-06-23 21:24:39 +0200

    new: Fetch data

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,12 @@
 import {getCurrentYear} from "./util/current-year";
+import {http} from "./server/http";
 
-// const currentYear = new Date().getFullYear();
-// const getCurrentYear = () => {
-//     document.getElementById('year').textContent = currentYear;
-// };
-console.log(getCurrentYear);
-//debugger
+// Get posts on Dom load
+function getProducts() {
+    http.get('http://localhost:3000/products')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+document.addEventListener('DOMContentLoaded', getProducts);
+
 getCurrentYear();
