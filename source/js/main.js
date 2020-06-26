f505cae67c0eb27900d91e33e45ebd111ca34f0a
sne <snezhana.syngur@gmail.com>
2020-06-26 16:05:57 +0200

    fix: Update packages

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,12 @@
 import {getCurrentYear} from "./util/current-year";
-import {http} from "./server/http";
+//import {http} from "./server/http";
 
-// Get posts on Dom load
-function getProducts() {
-    http.get('http://localhost:3000/products')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
-}
-document.addEventListener('DOMContentLoaded', getProducts);
+// // Get posts on Dom load
+// function getProducts() {
+//     http.get('http://localhost:3000/products')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+// }
+// document.addEventListener('DOMContentLoaded', getProducts);
 
-getCurrentYear();
+ getCurrentYear();
--- source/js/server/http.js
+++ source/js/server/http.js
@@ -1,56 +1,56 @@
-class EasyHTTP {
-    get(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => resolve(data))
-                .catch(err => reject(err));
-        });
-    }
+// class EasyHTTP {
+//     get(url) {
+//         return new Promise((resolve, reject) => {
+//             fetch(url)
+//                 .then(res => res.json())
+//                 .then(data => resolve(data))
+//                 .catch(err => reject(err));
+//         });
+//     }
 
-    post(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-                .then(res => res.json())
-                .then(data => resolve(data))
-                .catch(err => reject(err));
-        });
-    }
+//     post(url, data) {
+//         return new Promise((resolve, reject) => {
+//             fetch(url, {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-type': 'application/json'
+//                 },
+//                 body: JSON.stringify(data)
+//             })
+//                 .then(res => res.json())
+//                 .then(data => resolve(data))
+//                 .catch(err => reject(err));
+//         });
+//     }
 
-    put (url, data) {
-        return new Promise ((resolve, reject) => {
-            fetch(url, {
-                method: 'PUT',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-        });
-    }
+//     put (url, data) {
+//         return new Promise ((resolve, reject) => {
+//             fetch(url, {
+//                 method: 'PUT',
+//                 headers: {
+//                     'Content-type': 'application/json'
+//                 },
+//                 body: JSON.stringify(data)
+//             })
+//             .then(res => res.json())
+//             .then(data => resolve(data))
+//             .catch(err => reject(err));
+//         });
+//     }
 
-    delete (url) {
-        return new Promise ((resolve, reject) => {
-            fetch(url, {
-                method: 'DELETE',
-                headers: {
-                  'Content-type': 'application/json'
-                }
-            })
-            .then(res => res.json())
-            .then(() => resolve('Resource Deleted...'))
-            .catch(err => reject(err));
-        });
-    }
-}
+//     delete (url) {
+//         return new Promise ((resolve, reject) => {
+//             fetch(url, {
+//                 method: 'DELETE',
+//                 headers: {
+//                   'Content-type': 'application/json'
+//                 }
+//             })
+//             .then(res => res.json())
+//             .then(() => resolve('Resource Deleted...'))
+//             .catch(err => reject(err));
+//         });
+//     }
+// }
 
-export const http = new EasyHTTP();
+// export const http = new EasyHTTP();
