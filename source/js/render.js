23b30e3170fe5b833493fc5be4d11ecc4f4e01ec
sne <snezhana.syngur@gmail.com>
2020-07-02 11:19:31 +0200

    new: Fill products cards

--- source/js/render.js
+++ source/js/render.js
@@ -1,4 +1,6 @@
-import { removeCards } from './card';
+import {
+    removeCards
+} from './card';
 
 const COUNT_CARD_ON_PAGE = 9;
 const cardsList = document.querySelector('.cards-flow');
@@ -7,10 +9,19 @@ const cardTemplate = document.querySelector('#shop-card');
 const renderCard = function (card) {
     const cardElement = cardTemplate.content.cloneNode(true);
     const element = cardElement.querySelector(`.card`);
+    const newMark = element.querySelector('#card-mark');
+    const availability = element.querySelector('.card-order-mark');
+    if (card.isNew) {
+        newMark.classList.add('card-new-mark');
+        newMark.setAttribute("aria-label", "New item");
+    }
     element.querySelector('.card-img-top').src = `img/product-${card.id}.jpg`;
-
+    element.querySelector('.card-title').textContent = `${card.brand}`;
+    availability.textContent = card.isInStock ? `In Stock` : `Special Order`;
+    element.querySelector('.card-text').textContent = `${card.model}`;
+    element.querySelector('.card-price').textContent = `$ ${card.price}`;
     element.querySelector('.btn').addEventListener(`click`, function () {
-        console.log('click on element');
+        console.log('click-plick');
     });
 
     return element;
